297462796a95207832c8fc95f99cb11f8e5cded9
Wanitcha <u6213365@au.edu>
2024-07-28 10:55:07 +0700

    Remove MonoBehaviour inheritance

--- Assets/Scripts/Game Data Managers/GameData.cs
+++ Assets/Scripts/Game Data Managers/GameData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class GameData : MonoBehaviour
+public class GameData
 {
     public int score;
     public int matches;
