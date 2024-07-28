66d2f1ec7ea3a47a97c0d6dfe69fd60779e82f8b
Wanitcha <u6213365@au.edu>
2024-07-28 11:35:26 +0700

    Implement loading data from file

--- Assets/Scripts/GameDataManagers/FileDataHandler.cs
+++ Assets/Scripts/GameDataManagers/FileDataHandler.cs
@@ -15,6 +15,33 @@ public class FileDataHandler
         this.dataFileName = dataFileName;
     }
 
+    public GameData LoadFromFile()
+    {
+        string fullPath = Path.Combine(dataDirtyPath, dataFileName);
+        GameData loadedData = null;
+        if (File.Exists(fullPath))
+        {
+            try
+            {
+                string dataToLoad = "";
+                using (FileStream stream = new FileStream(fullPath, FileMode.Open))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        dataToLoad = reader.ReadToEnd();
+                    }
+                }
+
+                loadedData = JsonUtility.FromJson<GameData>(dataToLoad);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error occured when trying to load data from file: " + fullPath + "\n" + e);
+            }
+        }
+        return loadedData;
+    }
+
     public void SaveToFile(GameData data)
     {
         string fullPath = Path.Combine(dataDirtyPath, dataFileName);
