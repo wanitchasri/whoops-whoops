c6fbbf8a8424fdc22a3da69dc08941ad7b00869b
Wanitcha <u6213365@au.edu>
2024-07-28 10:36:38 +0700

    Initialize game data

---
+++ Assets/Scripts/Game Data Managers/GameData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData : MonoBehaviour
+{
+    public int score;
+    public int matches;
+    public int turns;
+    public List<Sprite> cardPictures;
+    public List<int> cardMatchInfo;
+
+    // default values
+    public GameData()
+    {
+        this.score = 0;
+        this.matches = 0;
+        this.turns = 0;
+        this.cardPictures = new List<Sprite>();
+        this.cardMatchInfo = new List<int>();
+    }
+}
